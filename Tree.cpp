8f350eb6ea73a96537a0f158008a661be71701c4
YOM1I <kalunino@gmail.com>
2025-05-05 22:22:58 +0200

    Add files via upload

---
+++ Tree.cpp
@@ -0,0 +1,852 @@
+﻿#include "Tree.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <stack>
+
+void Tree::delete_user(long long x)
+{
+	cvor* temp = search(x);
+
+	if (!temp) {
+		std::cout << "User does not exist" << std::endl;
+		return;
+	}
+
+	std::stack<cvor*> st;
+	st.push(temp);
+
+	while (!st.empty()) {
+
+		temp = st.top();
+		st.pop();
+
+
+		if (is_leaf(temp) || temp->father == root) {
+
+			for (int i = 0; i < temp->keys.size(); i++) {
+				if (temp->keys[i]->id == x) {
+					delete temp->keys[i];
+					temp->keys.erase(temp->keys.begin() + i);
+
+				}
+			}
+
+
+			if (temp->keys.size() >= temp->min_keys) {
+				return;
+			}
+			else
+			{
+				cvor* brother = can_borrow(temp);
+
+				if (brother) {
+
+					if (brother == temp->right_brother || brother == temp->right_brother && brother == temp->right_brother->right_brother) {
+						while (temp != brother) {
+							brother = borrow_right(brother);
+
+						}
+					}
+					else if (brother == temp->left_brother || brother == temp->left_brother && brother == temp->left_brother->left_brother) {
+						while (temp != brother) {
+							brother = borrow_left(brother);
+
+						}
+					}
+				}
+				else
+				{
+					cvor * f = minimalizing(temp);
+					if (temp->keys.size() >= temp->min_keys) {
+						break;
+					}
+					if(f)st.push(f);
+
+				}
+			}
+			
+		}
+
+		else {
+
+			cvor* p = find_s(temp, x);
+			if(p)st.push(p);
+
+		}
+		correct_brothers_and_link_everything();
+	}
+}
+
+Tree::cvor* Tree::minimalizing(cvor* temp)
+{
+	if (!temp) {
+		std::cout << "Ivalid temp" << std::endl;
+		return nullptr;
+	}
+	
+	std::vector<info*> vec; 
+
+	cvor* right = nullptr;
+	cvor* left = nullptr;
+
+	if (temp->right_brother) {
+		right = temp->right_brother;
+	}
+	if (temp->left_brother) {
+		left = temp->left_brother;
+	}
+
+	if (temp->father->sons.size() <= 2) {
+
+		temp->father->sons.clear();
+
+		temp->father->keys.insert(temp->father->keys.begin(), temp->keys.begin(), temp->keys.end());
+
+		temp->father->sons.insert(temp->father->sons.begin(), temp->sons.begin(), temp->sons.end());
+
+		if (right) {
+			temp->father->keys.insert(temp->father->keys.begin(), right->keys.begin(), right->keys.end());
+			temp->father->sons.insert(temp->father->sons.begin(), right->sons.begin(), right->sons.end());
+			delete right;
+		}
+		else if (left) {
+			temp->father->keys.insert(temp->father->keys.begin(), left->keys.begin(), left->keys.end());
+			temp->father->sons.insert(temp->father->sons.begin(), left->sons.begin(), left->sons.end());
+			delete left;
+		}
+	
+		bubbleSort_keys(temp->father->keys);
+		if(!temp->father->sons.empty()) bubbleSort_sons(temp->father->sons);
+		delete temp;
+		
+		//// ovo ce mi biti ovberall a ne samo isleaf
+		
+		return nullptr;
+	}
+
+	if (!left ) {
+		left = temp;
+		
+		temp = temp->right_brother;
+		
+		if(temp->right_brother) right = temp->right_brother;
+	}
+	else if (!right) {
+
+		right = temp;
+		temp = temp->left_brother;
+		if(temp->left_brother)left = temp->left_brother;
+
+
+	}
+	cvor* father = temp->father;
+
+	int pos = father->keys.size() - 1;
+
+	
+	if (!temp->keys.empty()) {
+		for (int i = 0; i < father->keys.size(); i++) {
+			if (temp->keys.front()->id < father->keys[i]->id) {
+				pos = i;
+				break;
+			}
+		}
+	}
+
+	if (right) vec.insert(vec.end(), right->keys.begin(), right->keys.end());
+	if (left) vec.insert(vec.end(), left->keys.begin(), left->keys.end());
+	vec.insert(vec.end(), temp->keys.begin(), temp->keys.end());
+	vec.push_back(father->keys[pos]);
+	father->keys.erase(father->keys.begin() + pos);
+
+	
+	if (pos > 0) {
+		vec.push_back(father->keys[pos-1]);
+		father->keys.erase(father->keys.begin() + pos - 1);
+	}
+	bubbleSort_keys(vec);
+
+	temp->keys.clear();
+	if (left) left->keys.clear();
+	if (right) right->keys.clear();
+
+
+	int split = vec.size() / 2;
+
+	
+	temp->keys.insert(temp->keys.begin(), vec.begin(), vec.begin() + split);
+
+	
+	if (right) {
+		right->keys.insert(right->keys.begin(), vec.begin() + split + 1, vec.end());
+	}
+
+
+	father->keys.push_back(vec[split]);
+	bubbleSort_keys(father->keys);
+	
+	for (int i = 0; i < father->sons.size(); i++) {
+		if (father->sons[i] == left) {
+			father->sons.erase(father->sons.begin() + i);
+			break;
+		}
+	}
+	delete left;
+	
+	bubbleSort_sons(father->sons);
+	
+	link_everything(father);
+
+	return father;
+
+}
+
+Tree::cvor* Tree::borrow_right(cvor* temp)
+{
+	
+	cvor* brother = temp->left_brother;
+
+	cvor* father = temp->father;
+
+	int pos = father->keys.size() - 1;
+
+
+	if (!temp->keys.empty()) {
+		for (int i = 0; i < father->keys.size(); i++) {
+
+			if (temp->keys.front()->id < father->keys[i]->id) {
+				pos = i - 1;
+				break;
+			}
+		}
+	}
+
+	brother->keys.push_back(father->keys[pos]);
+
+	father->keys[pos] = temp->keys.front();
+
+	if(!temp->keys.empty())temp->keys.erase(temp->keys.begin());
+
+	temp = brother;
+
+	return temp;
+
+}
+
+Tree::cvor* Tree::borrow_left(cvor* temp)
+{
+
+	cvor* brother = temp->right_brother;
+
+
+	cvor* father = temp->father;
+
+	int pos = father->keys.size() - 1;
+
+
+	if (!temp->keys.empty()) {
+		for (int i = 0; i < father->keys.size(); i++) {
+
+			if (temp->keys.front()->id < father->keys[i]->id) {
+				pos = i;
+				break;
+			}
+		}
+	}
+
+	brother->keys.insert(brother->keys.begin(), father->keys[pos]);
+
+	father->keys[pos] = temp->keys.back();
+
+	if (!temp->keys.empty()) {
+		temp->keys.pop_back();
+	}
+	// ovdje puca nekad
+
+	temp = brother;
+
+	return temp;
+
+}
+
+void Tree::insert_node(info* inf)
+{
+	std::stack<cvor*> st;
+
+	if (!root) {
+		cvor* cv = new cvor(root_keys_max, root_sons_max, root_keys_min);
+
+		cv->father = nullptr;
+
+		cv->keys.push_back(inf);
+
+		cv->right_brother = nullptr;
+		cv->left_brother = nullptr;
+
+		root = cv;
+
+		cv->left_brother = nullptr;
+		cv->right_brother = nullptr;
+
+		return;
+	}
+
+	cvor* target = search(inf->id);
+
+	insert_key(target, inf);
+
+	st.push(target);
+
+	while (!st.empty()) {
+
+		cvor* temp = st.top();
+		st.pop();
+		
+		if (is_overfull_node(temp)) {
+
+			
+			
+			if (temp == root) {
+				break_root(temp);
+				return;
+				
+			}
+
+			cvor* brother = can_overflow(temp);
+
+			if (!brother) {
+				std::cout << "NEMA BRATA ZA OBRADU" << std::endl;
+			}
+
+			if (brother && !is_full_node(brother)) {
+				overflow(temp, brother);
+				//prelivanje
+				// <- | ->
+			}
+			else {
+				
+				st.push(break_node(temp, brother));
+				
+
+			}		
+		}
+	}
+}
+
+Tree::cvor* Tree::break_node(cvor* temp, cvor* brother)
+{
+	std::vector<info*> vec;
+	std::vector<cvor*> vec_son;
+
+	cvor* father = temp->father;
+	int split;
+
+	cvor* lower;
+	cvor* higher;
+
+	if (temp->left_brother == brother) {
+
+		lower = brother;
+		higher = temp;
+	}
+	else{
+		
+		lower = temp;
+		higher = brother;
+	}
+
+
+	cvor* new_node = new cvor(all_keys_max, all_sons_max, all_keys_min);
+
+	int pos = father->keys.size() - 1; 
+
+	if (!temp->keys.empty()) {
+		for (int i = 0; i < father->keys.size(); i++) {
+
+			if (temp->keys.front()->id < father->keys[i]->id) {
+				pos = i; 
+				break; 
+			}
+		}
+	}
+	
+
+	vec.insert(vec.begin(), temp->keys.begin(), temp->keys.end());
+	vec.insert(vec.begin(), brother->keys.begin(), brother->keys.end());
+	vec.push_back(father->keys[pos]);
+
+
+	father->keys.erase(father->keys.begin() + pos);
+	brother->keys.clear();
+	temp->keys.clear();
+
+	bubbleSort_keys(vec);
+
+
+	lower->keys.insert(lower->keys.begin(), vec.begin(), vec.begin() + f1);
+	new_node->keys.insert(new_node->keys.begin(), vec.begin() + f1 + 1, vec.begin() + f1 + f2 + 1 );
+	higher->keys.insert(higher->keys.begin(), vec.begin() + f1 + f2 + 2, vec.end());
+
+	father->keys.push_back(vec[f1]);
+	father->keys.push_back(vec[f1+f2+1]);
+
+	father->sons.push_back(new_node);
+
+	bubbleSort_keys(father->keys);
+	bubbleSort_sons(father->sons);
+
+	link_everything(father);
+
+
+	return father;
+
+}
+
+
+void Tree::overflow(cvor* temp, cvor*brother)
+{
+	std::vector<info*> vec;
+	cvor* father = temp->father;
+
+	cvor* lower;
+	cvor* higher;
+
+	if (temp->left_brother == brother) {
+
+		lower = brother;
+		higher = temp;
+	}
+	else {
+
+		lower = temp;
+		higher = brother;
+	}
+
+	int split;
+	int pos = father->keys.size() - 1; 
+
+	
+	if (!temp->keys.empty()) {
+		for (int i = 0; i < father->keys.size(); i++) {
+		
+			if (temp->keys.front()->id < father->keys[i]->id) {
+				pos = i; 
+				break;   
+			}
+		}
+	}
+	
+
+	vec.insert(vec.begin(), temp->keys.begin(), temp->keys.end());
+	vec.insert(vec.begin(), brother->keys.begin(), brother->keys.end());
+	vec.push_back(father->keys[pos]);
+	
+	brother->keys.clear();
+	temp->keys.clear();
+
+	bubbleSort_keys(vec);
+
+	split = vec.size() / 2;
+
+	
+
+	for (int i = 0; i < split; i++) {
+		lower->keys.push_back(vec[i]);
+	}
+
+	for (int i = split + 1; i < vec.size(); i++) {
+		higher->keys.push_back(vec[i]);
+	}
+
+	father->keys[pos] = vec[split];
+
+	correct_brothers_and_link_everything();
+	
+}
+
+void Tree::break_root(cvor* temp)
+{
+
+	cvor* left = new cvor(all_keys_max,all_sons_max, all_keys_min);
+	cvor* right = new cvor(all_keys_max, all_sons_max, all_keys_min);
+	int split;
+
+	std::vector<info*> vec;
+	std::vector<cvor*> vec_sons;
+	std::vector<cvor*>f_sons;
+
+	f_sons.insert(f_sons.begin(), temp->sons.begin(), temp->sons.end());
+	temp->sons.clear();
+
+	for (int i = 0; i < temp->keys.size(); i++) {
+
+		vec.push_back(temp->keys[i]);
+	}
+
+	bubbleSort_keys(vec);
+	temp->keys.clear();
+
+	split = vec.size()/2;
+
+	left->keys.insert(left->keys.begin(), vec.begin(), vec.begin() + split);
+
+	right->keys.insert(right->keys.begin(), vec.begin() + split + 1, vec.end());
+
+	vec_sons.push_back(left);
+
+	vec_sons.push_back(right);
+
+	bubbleSort_sons(vec_sons);
+
+	temp->keys.push_back(vec[split]);
+
+	for (int i = 0; i < temp->keys.size(); i++) {
+		temp->sons.push_back(vec_sons[i]);
+	}
+	temp->sons.push_back(vec_sons.back());
+
+	link_everything(temp);
+
+	if (!f_sons.empty()) {
+		for (int i = 0; i < f_sons.size() / 2; i++) {
+			left->sons.push_back(f_sons[i]);
+		}
+		link_everything(left);
+		for (int i = f_sons.size()/2; i < f_sons.size(); i++) {
+			right->sons.push_back(f_sons[i]);
+			
+		}
+		link_everything(right);
+	}
+
+}
+
+void Tree::write_tree() {
+	if (root == nullptr) return;  // Provera da li je stablo prazno
+
+	std::queue<cvor*> que;
+	que.push(root);
+
+	while (!que.empty()) {
+		int nodes_at_current_level = que.size();  // Broj čvorova na trenutnom nivou
+
+		for (int j = 0; j < nodes_at_current_level; j++) {
+			cvor* temp = que.front();
+			que.pop();
+
+			if (temp != nullptr) {
+				// Dodavanje svih sinova trenutnog čvora u red
+				for (size_t k = 0; k < temp->sons.size(); k++) {
+					que.push(temp->sons[k]);
+				}
+
+			
+				for (size_t k = 0; k < temp->keys.size(); k++) {
+					std::cout << temp->keys[k]->id << " ";
+				}
+				std::cout << " | ";
+			}
+		}
+		std::cout << std::endl;
+		std::cout << "---------------------------------------------" << std::endl;
+	}
+}
+
+
+Tree::cvor* Tree::search(long long x)
+
+{
+
+	steps = 1;
+
+	cvor* temp = root;
+
+	while (!is_leaf(temp)) {
+		long long min = 0;
+
+		for (size_t i = 0; i < temp->keys.size(); i++) {
+			
+			if (temp->keys[i] != nullptr && temp->keys[i]->id == x) {
+				return temp;
+			}
+
+			if (x > temp->keys.back()->id) {
+				if (temp->sons.size() > temp->keys.size()) {
+					temp = temp->sons[temp->keys.size()];
+				}
+				else {
+					std::cerr << "Neispravan pristup `sons`." << std::endl;
+					return nullptr;
+				}
+				break;
+			}
+
+			
+			if (min < x && x < temp->keys[i]->id) {
+				temp = temp->sons[i];
+				break;
+			}
+
+			
+			if (temp->keys[i] != nullptr) {
+				min = temp->keys[i]->id;
+			}
+			
+		}
+		steps++;
+	}
+
+	
+	for (size_t i = 0; i < temp->keys.size(); i++) {
+		if (temp->keys[i] != nullptr && temp->keys[i]->id == x) {
+
+			return temp;
+		}
+	}
+
+	return temp;
+
+
+}
+
+
+
+void Tree::read(std::string file_name)
+{
+
+
+	std::ifstream file(file_name);
+	if (!file) {
+		std::cout << "File ne postoji";
+		return;
+	}
+	std::string line;
+
+	while (std::getline(file, line)) {
+
+		line.append("|");
+
+
+		info* temp = new info;
+
+		temp->id = stoll(line.substr(0, line.find('|')));
+		line.erase(0, line.find('|') + 1);
+
+		temp->first_name = line.substr(0, line.find('|'));
+		line.erase(0, line.find('|') + 1);
+
+		temp->last_name = line.substr(0, line.find('|'));
+		line.erase(0, line.find('|') + 1);
+
+		temp->email = line.substr(0, line.find('|'));
+		line.erase(0, line.find('|') + 1);
+
+		temp->ad_id = stoll(line.substr(0, line.find('|')));
+
+
+		//std::cout << temp->id << " | " << temp->first_name << " | " << temp->last_name << " | " << temp->email << " | " << temp->ad_id << std::endl;
+
+		data.push_back(temp);
+
+
+	}
+
+	file.close();
+
+}
+
+void Tree::add_new_user()
+{
+
+	std::cout << "Unesite podatke: ";
+
+	std::string line;
+
+	std::getline(std::cin, line);
+
+	info* temp = new info;
+
+		temp->id = stoll(line.substr(0, line.find('|')));
+		line.erase(0, line.find('|') + 1);
+
+		temp->first_name = line.substr(0, line.find('|'));
+		line.erase(0, line.find('|') + 1);
+
+		temp->last_name = line.substr(0, line.find('|'));
+		line.erase(0, line.find('|') + 1);
+
+		temp->email = line.substr(0, line.find('|'));
+		line.erase(0, line.find('|') + 1);
+
+		temp->ad_id = stoll(line.substr(0, line.find('|')));
+
+	
+		std::cout << temp->id << " | " << temp->first_name << " | " << temp->last_name << " | " << temp->email << " | " << temp->ad_id << std::endl;
+
+		insert_node(temp);
+
+}
+
+void Tree::search_by_name()
+{
+
+	if (root == nullptr) return;
+
+
+	steps = 1;
+
+	std::cout << "Unesite ime: ";
+
+	std::string target;
+
+	std::getline(std::cin, target);
+
+
+	info* temp;
+
+
+
+	std::queue<cvor*> que;
+	que.push(root);
+
+	while (!que.empty()) {
+
+		cvor* processing = que.front();
+		que.pop();
+		for (int i = 0; i < processing->sons.size(); i++) {
+
+			que.push(processing->sons[i]);
+		}
+
+		for (int i = 0; i < processing->keys.size(); i++) {
+
+			if (processing->keys[i]->first_name == target) {
+
+				temp = processing->keys[i];
+				std::cout << "Steps: " << steps << std::endl;
+				std::cout << std::endl;
+				std::cout << temp->id << " | " << temp->first_name << " | " << temp->last_name << " | " << temp->email << " | " << temp->ad_id << std::endl;
+				std::cout << std::endl;
+				return;
+			}
+
+		}
+		steps++;
+
+
+
+	}
+
+	std::cout << "User does not exist" << std::endl;
+
+
+
+}
+
+Tree::info* Tree::search_by_id(long long x)
+{
+	
+
+	if (x == 0) {
+
+		std::cout << "Unesite id: ";
+
+		std::cin >> x;
+
+		std::cout << std::endl;
+	}
+
+
+
+	info* temp = nullptr;
+
+	cvor* target_node = search(x);
+
+	
+
+	for (int i = 0; i < target_node->keys.size(); i++) {
+
+		if (target_node->keys[i]->id == x) {
+
+			temp = target_node->keys[i];
+
+		}
+	}
+
+	if (!temp) {
+		std::cout << "Steps: " << steps << std::endl;
+		std::cout << std::endl;
+		return nullptr;
+	}
+	std::cout << "Steps: " << steps << std::endl;
+	std::cout << std::endl;
+	
+
+	return temp;
+
+
+}
+
+void Tree::output_id() {
+
+	info* temp = search_by_id();
+
+	if (!temp) {
+		std::cout << "User does not exist" << std::endl;
+		std::cout << std::endl;
+	}
+	else {
+		std::cout << temp->id << " | " << temp->first_name << " | " << temp->last_name << " | " << temp->email << " | " << temp->ad_id << std::endl;
+		std::cout << std::endl;
+
+
+	}
+
+}
+
+void Tree::search_multiple_keys(int k)
+{
+
+	int sum = 0;
+
+
+	std::vector<info*> output_arr;
+
+	std::vector<long long> arr;
+
+	long long x;
+
+	while (1) {
+
+		std::cout << "Unesite id: ";
+		std::cin >> x;
+		std::cout << std::endl;
+
+		if (x == 0) {
+			break;
+		}
+		arr.push_back(x);
+
+
+	}
+
+	for (int i = 0; i < arr.size(); i++) {
+		info* temp = search_by_id(arr[i]);
+		if (temp) {
+			output_arr.push_back(temp);
+			
+		}
+		sum += steps;
+
+
+	}
+
+	std::cout << sum << std::endl;
+	write_file(output_arr);
+
+
+
+}
+
---
+++ Tree.h
@@ -0,0 +1,379 @@
+﻿#pragma once
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <fstream>
+#include <iostream>
+
+
+class Tree{
+	public:
+
+		int m;
+
+		int all_sons_min;
+		int all_sons_max;
+		int all_keys_min;
+		int all_keys_max;
+
+		int root_sons_min;
+		int root_sons_max;
+		int root_keys_min;
+		int root_keys_max;
+
+		int f1;
+		int f2;
+		int f3;
+
+		int test;
+
+		int steps;
+
+		struct info
+		{
+			long long id = 0;
+			std::string first_name;
+			std::string last_name;
+			std::string email;
+			long long ad_id = 0;
+
+		};
+
+		struct cvor {
+			cvor* father = nullptr;
+			cvor* left_brother = nullptr;
+			cvor* right_brother = nullptr;
+			std::vector<info*> keys;
+			std::vector<cvor*> sons;
+			int max_keys;
+			int max_sons;
+
+			int min_keys;
+
+			cvor(double max_k, double max_s, double min_keys)
+				: father(nullptr), left_brother(nullptr), right_brother(nullptr),
+				max_keys(static_cast<int>(max_k)), max_sons(static_cast<int>(max_s)) , min_keys(static_cast<int>(min_keys)) {}
+		};
+
+		std::vector<info*> data;
+
+		cvor* root = nullptr;
+
+	public:
+
+		Tree(int par, std::string inf) {
+			m = par;
+
+			root_sons_min = 2;
+			root_sons_max = 2 * static_cast<int>(std::floor((2 * m - 2) / 3)) + 1;
+			root_keys_min = root_sons_min - 1;
+			root_keys_max = root_sons_max - 1;
+
+			all_sons_min = static_cast<int>(std::ceil((2 * m - 1) / 3));
+			all_sons_max = static_cast<int>(m);
+			all_keys_min = static_cast<int>(std::ceil((2 * m - 1) / 3));
+			all_keys_max = static_cast<int>(m - 1);
+
+			f1 = static_cast<int>(std::floor((2 * m - 2) / 3));
+			f2 = static_cast<int>(std::floor((2 * m - 1) / 3));
+			f3 = static_cast<int>(std::floor((2 * m) / 3));
+
+			read(inf);
+
+
+		}
+
+		//glavne funckije
+
+		void delete_user(long long x);
+
+		cvor* minimalizing(cvor*temp);
+
+		cvor* borrow_right(cvor* temp);
+
+		cvor* borrow_left(cvor* temp);
+
+		void insert_node(info* inf);
+
+		cvor* break_node(cvor* temp, cvor* brother);
+
+		void overflow(cvor* temp, cvor* brother);
+
+		void break_root(cvor* temp);
+
+		void write_tree();
+
+		cvor* search(long long x);
+
+		void read(std::string file_name);
+
+		
+
+		/// kratke funkcije
+
+
+		void generate_tree();
+		
+		bool is_leaf(cvor* temp);
+
+		bool is_full_node(cvor* temp);
+
+		bool is_overfull_node(cvor* temp);
+
+		cvor* can_overflow(cvor* temp);
+
+		void insert_key(cvor* temp, info* x);
+
+		void bubbleSort_keys(std::vector<info*>& vec);
+
+		void bubbleSort_sons(std::vector<cvor*>& vec);
+
+		void correct_brothers_and_link_everything();
+
+		void link_everything(cvor* temp);
+
+		cvor* can_borrow(cvor* temp);
+
+		cvor* find_s(cvor*temp, long long x);
+
+
+		//ostale tacke zadatka
+
+
+		void add_new_user();
+
+		info* search_by_id(long long x = 0);
+
+		void search_by_name();
+
+		void output_id();
+
+		void search_multiple_keys(int k);
+
+		void write_file(std::vector<info*> output_txt);
+
+	};
+
+	inline void Tree::generate_tree()
+	{
+		test = 5; 
+		for (int i = 0; i < test; i++) {
+
+			info* temp = data[i];
+
+			insert_node(temp);
+
+			correct_brothers_and_link_everything();
+		}
+	}
+
+
+	inline bool Tree::is_leaf(cvor* temp)
+	{
+		for (int i = 0; i < temp->max_sons; i++) {
+			if (temp->sons.size() != 0) return false;
+		}
+
+		return true;
+	}
+
+	inline bool Tree::is_full_node(cvor* temp)
+	{
+		return temp->keys.size() == temp->max_keys;
+	}
+	inline bool Tree::is_overfull_node(cvor* temp)
+	{
+		return temp->keys.size() > temp->max_keys;
+	}
+
+	inline Tree::cvor* Tree::can_overflow(cvor* temp)
+	{
+		if (temp && temp->right_brother != nullptr) {
+	
+			return temp->right_brother;
+		}
+		else if (temp && temp->left_brother != nullptr) {
+			
+			return temp->left_brother;
+		}
+		return nullptr;
+	}
+
+
+	inline void Tree::insert_key(cvor* temp, info* x) {
+		int pos = temp->keys.size();
+		temp->keys.resize(temp->keys.size() + 1, nullptr);
+
+		for (int i = 0; i < temp->keys.size() - 1; i++) {
+			if (temp->keys[i] != nullptr && x->id < temp->keys[i]->id) {
+				pos = i;
+				break;
+			}
+		}
+
+		for (size_t i = temp->keys.size() - 1; i > static_cast<size_t>(pos); i--) {
+			temp->keys[i] = temp->keys[i - 1];
+		}
+
+		temp->keys[pos] = x;
+	}
+
+	inline void Tree::bubbleSort_keys(std::vector<info*>& vec) {
+		for (size_t i = 0; i < vec.size() - 1; i++) {
+			bool swapped = false;
+			for (size_t j = 0; j < vec.size() - i - 1; j++) {
+			
+				if (vec[j]->id > vec[j + 1]->id) {
+					std::swap(vec[j], vec[j + 1]);
+					swapped = true;
+				}
+			}
+			if (!swapped) break;
+		}
+	}
+
+
+	inline void Tree::bubbleSort_sons(std::vector<cvor*>& vec) {
+		
+		for (size_t i = 0; i < vec.size() - 1; i++) {
+			bool swapped = false;
+			for (size_t j = 0; j < vec.size() - i - 1; j++) {
+				if (vec[j]->keys.back()->id > vec[j + 1]->keys.back()->id) {
+					std::swap(vec[j], vec[j + 1]);
+					swapped = true;
+				}
+			}
+			if (!swapped) break;
+		}
+	}
+
+
+	inline void Tree::link_everything(cvor* temp)
+	{
+		for (int i = 0; i < temp->sons.size(); i++) {
+			if (i == temp->sons.size() - 1) {
+				temp->sons[i]->right_brother = nullptr;
+			}
+			else {
+				temp->sons[i]->right_brother = temp->sons[i + 1];
+			}
+		}
+
+		for (int i = 0; i < temp->sons.size(); i++) {
+			if (i == 0) {
+				temp->sons[i]->left_brother = nullptr;
+			}
+			else {
+				temp->sons[i]->left_brother = temp->sons[i - 1];
+			}
+		}
+		for (int i = 0; i < temp->sons.size(); i++) {
+			temp->sons[i]->father = temp;
+		}
+	}
+
+	inline Tree::cvor* Tree::can_borrow(cvor* temp)
+	{
+		if (temp->right_brother && temp->right_brother->keys.size() > temp->right_brother->min_keys) return temp->right_brother;
+		else if (temp->left_brother && temp->left_brother->keys.size() > temp->left_brother->min_keys) return temp->left_brother;
+		else if(temp->right_brother && temp->right_brother->right_brother && temp->right_brother->right_brother->keys.size() > temp->right_brother->right_brother->min_keys)
+			return temp->right_brother->right_brother;
+		else if (temp->left_brother && temp->left_brother->left_brother && temp->left_brother->left_brother->keys.size() > temp->left_brother->left_brother->min_keys) 
+			return temp->left_brother->left_brother;
+
+
+		return nullptr;
+	}
+
+	inline Tree::cvor* Tree::find_s(cvor* temp, long long x)
+
+	{
+		
+		cvor* p = nullptr;
+
+		int pos = temp->keys.size() - 1;
+
+		for (int  i = 0; i < temp->keys.size(); i++) {
+
+			if (temp->keys[i]->id == x) {
+
+				pos = i;
+				break;
+			}
+
+		}
+		p = temp->sons[pos+1];
+
+		while (!is_leaf(p)) {
+			p = p->sons.front();
+		}
+		temp->keys[pos] = p->keys.front(); // pos
+
+		p->keys.erase(p->keys.begin());
+
+
+
+
+		return p;
+	}
+
+	inline void Tree::correct_brothers_and_link_everything()
+	{
+		
+		std::queue<cvor*> st;
+
+		cvor* temp = root;
+
+		st.push(temp);
+
+		while (!st.empty()) {
+
+			temp = st.front();
+			st.pop();
+			link_everything(temp);
+
+			while (!temp->sons.empty() && !temp->keys.empty() && (temp->sons.size() > temp->keys.size() + 1 || temp->sons.size() > temp->max_sons) && temp != root) {
+				if (temp->right_brother != nullptr) {
+
+					temp->right_brother->sons.insert(temp->right_brother->sons.begin(), temp->sons.back());
+					temp->sons.pop_back(); 
+					link_everything(temp->right_brother);
+
+				}
+				else if (temp->left_brother != nullptr) {
+					
+					temp->left_brother->sons.push_back(temp->sons.front());
+					temp->sons.erase(temp->sons.begin());
+					link_everything(temp->left_brother);
+				}
+			}
+
+			link_everything(temp);
+
+			for (int i = 0; i < temp->sons.size(); i++) {
+				st.push(temp->sons[i]);
+			}
+
+		}
+	}
+
+	inline void Tree::write_file(std::vector<info*> output_txt) {
+
+		std::ofstream file("output.txt");
+
+		for (int i = 0; i < output_txt.size(); i++) {
+			info* temp = output_txt[i];
+
+			file << temp->id << " | " << temp->first_name << " | " << temp->last_name << " | " << temp->email << " | " << temp->ad_id << std::endl;
+
+		}
+		file.close();
+
+	}
+
+
+
+
